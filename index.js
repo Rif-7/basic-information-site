f70f6cf392597b210f4c2c218ec021af0a9cf789
Rif-7 <muhammadrifan974530@gmail.com>
2022-10-13 22:01:16 +0530

    add about, contact-me and 404 routes

--- index.js
+++ index.js
@@ -8,11 +8,37 @@ const server = http.createServer((req, res) =>{
             if (err) {
                 throw err
             }
-            res.writeHead(200, {"Content-Type": "text/html"});
             res.write(data);
             res.end()
         })
-  
+    
+    } else if (req.url == "/about") {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        fs.readFile("about.html", (err, data) => {
+            if (err) {
+                throw err;
+            }
+            res.write(data);
+            res.end();
+        })
+    } else if (req.url == "/contact-me") {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        fs.readFile("contact-me.html", (err, data) => {
+            if (err) {
+                throw err;
+            }
+            res.write(data);
+            res.end();
+        })
+    } else {
+        res.writeHead(404, {"Content-Type": "text/html"});
+        fs.readFile("404.html", (err, data) => {
+            if (err) {
+                throw err;
+            }
+            res.write(data);
+            res.end();
+        })
     }
 });
 
